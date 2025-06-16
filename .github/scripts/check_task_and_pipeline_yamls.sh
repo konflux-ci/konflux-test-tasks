5420d33cbbf417eaf775e5790496d1059bb7910e
Hongwei Liu <hongliu@redhat.com>
2025-06-16 12:11:40 +0800

    Update check files and renovate json file
    update .github/workflows/check-task-and-pipeline-yamls.yaml
    delete .github/workflows/check-readmes.yaml
    update renovate.json
    
    Signed-off-by: hongliu <hongliu@redhat.com>

--- .github/scripts/check_task_and_pipeline_yamls.sh
+++ .github/scripts/check_task_and_pipeline_yamls.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# copied from .github/workflows/check-task-and-pipeline-yamls.yaml of https://github.com/konflux-ci/build-definitions
 shopt -s nullglob
 set -euo pipefail
 
@@ -15,42 +17,5 @@ for task_folder in task/*/; do
         fi
       done
     )
-  fi
-done
-
-echo ">>> Apply pipelines"
-
-cd pipelines
-
-ignored_pipelines=(
-  "template-build"
-)
-
-for pipeline in */; do
-  if [ -d "$pipeline" ]; then
-    pipeline="$(basename "$pipeline")"
-    for ignored in "${ignored_pipelines[@]}"; do
-      if [ "$ignored" == "$pipeline" ]; then
-        echo ">>> Ignoring pipeline: $pipeline"
-        continue 2
-      fi
-    done
-    echo ">>> Pipeline: $pipeline"
-    to_apply=()
-    for yaml in "$pipeline"/*.yaml; do
-      if [ -f "$yaml" ] && yq eval -e 'type == "!!map" and .kind == "Pipeline"' "$yaml" > /dev/null 2>&1; then
-        to_apply+=("$yaml")
-      fi
-    done
-    
-    for file in "${to_apply[@]}"; do
-      # Pipeline files contain a version field (that is not a part of the pipeline spec) in the taskRef
-      # that is supposed to get processed, and then removed by the hack/build-and-push.sh script. 
-      # We don't call that script and for our purposes we can just remove the version field. 
-      # Otherwise the pipeline would get rejected by kubectl as it would not be valid.
-      yq eval --inplace "del(.spec.tasks[].taskRef.version)" "$file"
-      yq eval --inplace "del(.spec.finally[].taskRef.version)" "$file"
-      kubectl apply -f "$file" --dry-run=server
-    done
   fi
 done
